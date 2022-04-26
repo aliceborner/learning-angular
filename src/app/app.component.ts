ba0dca677c3e9c5220387f26d6f8ddde81097e3d
Alice Borner <aliceborner@aliceborner.fritz.box>
2022-04-26 08:02:30 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+// LEARNING this is like us creating our own html tag :)
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // LEARNING data binding - used in the template (html) to output dynamic content 
   title = 'my-first-angular-app';
+  name = "Alice";
 }
