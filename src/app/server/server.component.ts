2f12c1eedda5b40fc08569d976ae4a8d478dd4fb
Alice Borner <alicezbo@gmail.com>
2022-05-03 08:49:26 +0200

    Adding examples for ngStyle and ngClass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,14 +1,34 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-server',
-    templateUrl: './server.component.html'
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styles: [
+    `
+      .online {
+        color: white;
+      }
+    `,
+  ],
 })
 export class ServerComponent {
-    serverId = 10;
-    serverName = 'test';
+  serverId = 10;
+  serverName = 'test';
+  serverStatus: string = 'offline';
 
-    getServerName() {
-        this.serverName;
-    }
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
+  getServerName() {
+    return this.serverName;
+  }
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
+  }
 }
